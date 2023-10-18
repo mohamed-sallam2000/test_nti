ea8a3d0c0fc4fb5122cee6010ff639129dacd90e
mohamedsallam <mohamedsallam899@gmail.com>
2023-10-18 15:30:35 +0300

    should  be  replaced
    text

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    printf("A^D=%d\n",'A'^'D');
+
+    printf("F^B=%d\n",'B'^'F');
+
+    printf("G^C=%d\n",'C'^'G');
+
+    printf("Y^A=%d\n",'A'^'Y');
+
+    printf("L^B=%d\n",'B'^'L');
+
+    printf("E^C=%d\n",'C'^'E');
     return 0;
 }
